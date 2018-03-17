9c517a365229aeb285f86040c6c3448fb27c4add
David Chiu <davidwchiu@gmail.com>
2018-03-17 20:10:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
 end
-return "HAPPY NEW YEAR"
+"HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -15,5 +15,5 @@ def countdown_with_sleep(number)
     number -= 1
     sleep 1
 end
-return "HAPPY NEW YEAR"
+"HAPPY NEW YEAR!"
 end
